adc7ccde4809d4e7e8a078e8bfca96fa9455adc9
Joel Watkins <eck087@yahoo.co.uk>
2022-11-03 16:03:34 +0000

    added one more component

--- src/Content.js
+++ src/Content.js
@@ -1,18 +1,16 @@
-
 import EmailBody from "./Email-Body";
 import EmailHeader from "./Email-Header";
+import EmailActions from "./Email-Actions";
+
 function Content() {
   return (
     <article className="email-content">
       <div className="title">
         <h1>Welcome to Flaticon</h1>
       </div>
-        <EmailHeader/>
-        <EmailBody/>
-      <section className="email-actions">
-        <button>Reply</button>
-        <button>Forward</button>
-      </section>
+      <EmailHeader />
+      <EmailBody />
+      <EmailActions />
     </article>
   );
 }
